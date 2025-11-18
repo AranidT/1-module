d54e31323fd8647e68b0c168eb34a6597e634a05
Di <dinarazamatova@gmail.com>
2025-11-18 15:17:56 +0500

    passed 25-task

---
+++ 25-task.js
@@ -0,0 +1,70 @@
+// 1-task
+// function greet(message){
+//     console.log(message)
+// }
+// greet('Привет, мир!')
+
+// 2-task
+// function sayHello(name){
+//     console.log('Привет, ', name)
+// }
+// sayHello('Di')
+// sayHello('Adele')
+// sayHello('John')
+
+// 3-task
+// function sumTwoNumber(number1, number2){
+//     return number1+number2;
+// }
+// console.log(sumTwoNumber(1, 2))
+
+// 4-task
+// function isEven(number){
+//     console.log((number%2==0)?'true':'false')
+// }
+// isEven(5)
+// isEven(7)
+// isEven(100)
+
+// 5-task
+// const maxValue = function(number1, number2){
+//     if (number1>number2){
+//         return number1
+//     } else if (number1<number2){
+//         return number2
+//     } else{
+//         return 'Они одинаковы'
+//     }
+// }
+// console.log(maxValue(5,5))
+
+
+// 6-task
+// const getInitials = function(name, surname){
+//     console.log(name[0]+'.'+surname[0]+'.')
+// }
+// getInitials('Dinara', 'Azamatova')
+
+// 7-task
+// function square(number){
+//     return number ** 2
+// }
+
+// function cub(number){
+//     return number ** 3
+// }
+// console.log(cub(square(2)))
+
+// 8-task
+// sum = (number1, number2) => number1 + number2;
+// console.log(sum(5,6))
+
+// additional task
+// getFactorial = (number) => {
+//     if (number === 1) {
+//         return 1;
+//     } else {
+//         return number * getFactorial(number-1);
+//     }
+// }
+// console.log(getFactorial(5))
