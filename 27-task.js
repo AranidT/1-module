98eac7beb50cbc8ba2571c0f5d244df76b24f078
Di <dinarazamatova@gmail.com>
2025-11-20 10:17:02 +0500

    passed 27-task

---
+++ 27-task.js
@@ -0,0 +1,50 @@
+// task 1
+// let fruits = ['Яблоко', 'Банан', 'Апельсин']
+// console.log(fruits)
+
+// task 2
+// console.log(fruits[0], fruits[2])
+// fruits.push('Груша')
+// console.log(fruits)
+
+// task 3
+// fruits.pop()
+// fruits.shift()
+// console.log(fruits)
+
+// task 4
+// fruits.forEach(function(fruit){
+//     console.log(fruit)
+// })
+
+// task 5
+// let lengths = fruits.map(fruit => fruit.length)
+// console.log(lengths)
+
+// task 6
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// let even = numbers.filter(num => num % 2 == 0)
+// console.log(even)
+
+// task 7
+// let sum = numbers.reduce((acc, num) => acc + num,0)
+// console.log(sum)
+
+// task 8
+// const firstItem = numbers.find(function(firsty){
+//     return firsty > 5
+// })
+
+// console.log(firstItem)
+
+// task 9
+// let arr1 = [1, 2, 3]
+// let arr2 = [4, 5, 6]
+// let arr3 = arr1.concat(arr2)
+// console.log(arr3)
+
+// task 10
+// console.log(fruits.includes('Банан'))
+
+// task 11
+// console.log(fruits.reverse())
