03c522fff5f2d9fa8de72c0aacfa3037371c2893
Di <dinarazamatova@gmail.com>
2025-11-22 09:45:55 +0500

    passed 28-task

---
+++ 28-task.js
@@ -0,0 +1,91 @@
+// task 1
+// let i = 1
+// while (i < 11){
+//     console.log(i)
+//     i++
+// }
+
+// task 2
+// let count = 1
+// while (count < 11){
+//     console.log(count)
+//     count++  
+//     if (count == 5){
+//         continue;
+//     }
+//     else if (count == 8){
+//         break;
+//     } 
+// }
+
+// task 3
+// count = 1
+// do {
+//     console.log(count)
+//     count++
+// } while(count < 6)
+// do while выведит результат хотя б 1 раз даже если условие ложное, while не выведит
+
+// task 4
+// count = prompt('Введите число')
+// while (true){
+//     console.log(count)
+//     if (count==10){
+//         break
+//     }
+// }
+
+// task 5 
+// for(let i = 1; i < 11; i++){
+//     console.log(i)
+// }
+
+// task 6
+// let i = 1
+// for(;;){
+//     console.log(i)
+//     i++
+//     if (i > 100){
+//         break;
+//     }
+// }
+
+// task 7
+// numbers = [1, 2, 3, 4, 5]
+// for(let i = 0; i < numbers.length; i++){
+//     console.log(numbers[i])
+// }
+
+// task 8
+// for если мы знаем конечную итерацию, на чем заканчивается цикл, а while если не знаем
+
+// пример цикла for например для трекера дня недели
+// for(let i = 1; i < 8; i++){
+//     console.log('День: ', i)
+// }
+
+// пример цикла while запрос пароля пока он не введет верный
+// let input = prompt('Введите пароль: ')
+// while(true){
+//     if (input == 'qWer122!'){
+//         break;
+//     }
+//     else{
+//         console.log('Вы ввели неверный пароль!')
+//     }
+// }
+
+// task 9
+// for(let i = 10; i > 0; i--){
+//     console.log(i)
+// }
+
+// task 10
+// for(let i = 1; i < 11; i++){
+//     if (i % 2 == 0){
+//         console.log(i)
+//     }
+//     else{
+//         continue;
+//     }
+// }
